c1eff5b0f31c0a1559d6e2093baf5e260eb6875b
Tamsin Huggins <tamsin.hns@gmail.com>
2023-07-03 15:55:28 +0100

    working dash app, user selects dimension/variable to view over time

--- appEducation.py
+++ appEducation.py
@@ -6,71 +6,68 @@ from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0
 
 app = Dash(__name__)
 
-# df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Other/Dash_Introduction/intro_bees.csv")
-
-# df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
-# df.reset_index(inplace=True)
-# print(df[:5])
 
 # ------------------------------------------------------------------------------
 # App layout
+
+df=pd.read_csv('with_iso_alpha_education.csv')
+
 app.layout = html.Div([
 
     html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
 
-    dcc.Dropdown(id="slct_year",
+    dcc.Dropdown(id="slct_view",
                  options=[
-                     {"label": "2015", "value": 2015},
-                     {"label": "2016", "value": 2016},
-                     {"label": "2017", "value": 2017},
-                     {"label": "2018", "value": 2018}],
+                     {"label": "Completed Primary Education", "value": "completed_primary"},
+                     {"label": "Completed Secondary Education", "value": "completed_secondary"},
+                     {"label": "Completed Teritary Education", "value":"completed_tertiary"},
+                     {"label": "Population (Thousands)", "value": "population_1000s"},
+                     {"label": "Average Years Total Schooling", "value": "avg_years_total_schooling" }],
                  multi=False,
-                 value=2015,
+                 value="avg_years_total_schooling",
                  style={'width': "40%"}
                  ),
 
     html.Div(id='output_container', children=[]),
     html.Br(),
 
-    dcc.Graph(id='my_bee_map', figure={})
+    dcc.Graph(id='education_map', figure={})
 
 ])
 
-
 # ------------------------------------------------------------------------------
 # Connect the Plotly graphs with Dash Components
-# @app.callback(
-#     [Output(component_id='output_container', component_property='children'),
-#      Output(component_id='my_bee_map', component_property='figure')],
-#     [Input(component_id='slct_year', component_property='value')]
-# )
-# def update_graph(option_slctd):
-#     print(option_slctd)
-#     print(type(option_slctd))
-
-#     container = "The year chosen by user was: {}".format(option_slctd)
-
-#     dff = df.copy()
-#     dff = dff[dff["Year"] == option_slctd]
-#     dff = dff[dff["Affected by"] == "Varroa_mites"]
-#     print(dff.head())
-
-    # Plotly Express
-    # fig = px.choropleth(
-    #     data_frame=dff,
-    #     locationmode='USA-states',
-    #     locations='state_code',
-    #     scope="usa",
-    #     color='Pct of Colonies Impacted',
-    #     hover_data=['State', 'Pct of Colonies Impacted'],
-    #     color_continuous_scale=px.colors.sequential.YlOrRd,
-    #     labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
-    #     template='plotly_dark',
-    #     range_color=[1,100]
-    # )
+@app.callback(
+    [Output(component_id='output_container', component_property='children'),
+     Output(component_id='education_map', component_property='figure')],
+    [Input(component_id='slct_view', component_property='value')] # must be value not label
+)
+def update_graph(option_slctd):
+    # retrieves information from Input to select data from df
 
 
-    # return container, fig
+    app.logger.info(option_slctd)
+    print(option_slctd)
+    print(type(option_slctd))
+
+    container = "You are viewing: {}".format(option_slctd)
+
+    # dff = df.copy()
+    # dff = dff[['year', option_slctd]]
+    # print(dff.head())
+
+    # // Plotly Express
+    fig = px.choropleth(
+                        data_frame=df,
+                        animation_frame='year',
+                        color=option_slctd, 
+                        locations="iso_alpha",
+                        color_continuous_scale=px.colors.diverging.Geyser_r,
+                        hover_data= ['country'],
+                        range_color=[0, df[option_slctd].max()] # calibrate the scale to the right range
+                        )
+
+    return container, fig
 
 
 # ------------------------------------------------------------------------------
